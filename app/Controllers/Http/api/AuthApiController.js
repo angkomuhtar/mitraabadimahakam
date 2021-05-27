775bc306bc4c1b3f0765cf5aa37b4ab6868919ed
Mitra Abadi Mahakam <>
2021-05-27 10:36:45 +0800

    add user data on login api

--- app/Controllers/Http/api/AuthApiController.js
+++ app/Controllers/Http/api/AuthApiController.js
@@ -2,19 +2,34 @@
 const { performance } = require('perf_hooks')
 const diagnoticTime = use("App/Controllers/Http/customClass/diagnoticTime")
 
+const userx = use("App/Models/VUser")
+
 class AuthApiController {
     async login ({request, auth, response}) {
         var t0 = performance.now()
         const { username, password } = request.all()
+        let durasi
         try {
             const token = await auth.authenticator('jwt').attempt(username, password)
-            const durasi = await diagnoticTime.durasi(t0)
+            const usr = await userx.findBy('username', username)
+            durasi = await diagnoticTime.durasi(t0)
             return response.status(201).json({
-                data: token
+                diagnostic: {
+                    times: durasi, 
+                    error: false,
+                },
+                data: token,
+                user: usr.toJSON()
             })
         } catch (error) {
             console.log(error);
+            durasi = await diagnoticTime.durasi(t0)
             return response.status(404).json({
+                diagnostic: {
+                    times: durasi, 
+                    error: true,
+                    message: error.message
+                },
                 data: null
             })
         }
