cdf783632e42c06bcc6f9240ee6063232454f390
Mitra Abadi Mahakam <>
2021-06-03 13:32:43 +0800

    fix you

--- app/Controllers/Http/api/EquipmentApiController.js
+++ app/Controllers/Http/api/EquipmentApiController.js
@@ -3,6 +3,7 @@
 // CustomClass
 const moment = require('moment')
 const { performance } = require('perf_hooks')
+const { now } = require('underscore')
 const diagnoticTime = use("App/Controllers/Http/customClass/diagnoticTime")
 
 const MasShift = use("App/Models/MasShift")
@@ -59,7 +60,9 @@ class EquipmentApiController {
 
     async availableForFleet ({auth, request, response}) {
         var t0 = performance.now()
-        const dateReq = new Date()
+        const req = request.only(['datetime'])
+        const dateReq = new Date(req.datetime)
+
         try {
             await auth.authenticator('jwt').getUser()
         } catch (error) {
@@ -75,34 +78,48 @@ class EquipmentApiController {
             })
         }
         
-        const filterTime = moment(dateReq).format('HH:mm:ss')
-        let equipment_id = []
-        let data = []
+        const filterDate = moment(dateReq).format('YYYY-MM-DD')
+        // const filterTime = moment(dateReq).format('HH:mm')
+        const listShift = (await MasShift.query().select('id', 'name', 'kode', 'start_shift', 'end_shift').fetch()).toJSON()
+        const shiftData = listShift.map(item => {
+            var start = `${ filterDate } ${ item.start_shift }`
+            var end = `${moment(`${filterDate} ${item.start_shift}`).add(12, 'h')}`
+            if(new Date(start) < new Date(dateReq) && new Date(end) > new Date(dateReq)){
+                return {
+                    ...item,
+                    status: true,
+                    start_shift: start,
+                    end_shift: end
+                }
+            }else{
+                return {
+                    ...item,
+                    status: false,
+                    start_shift: start,
+                    end_shift: end
+                }
+            }
+        })
+
+        const [ShiftFilter] = shiftData.filter(item => item.status)
         try {
-            const shiftClock = 
-                await MasShift.query()
-                .where(
-                    builder => 
-                    builder.where('start_shift', '<=', filterTime)
-                    .where('end_shift', '>', filterTime)
-                    .where({status: 'Y'}))
-                    .first()
 
             const dailyFleet = (
                 await DailyFleet.query()
-                    .with('details')
-                    .where('shift_id', shiftClock.id)
-                    .fetch()
-                ).toJSON()
-
-
+                .with('details')
+                .where('shift_id', ShiftFilter.id)
+                .fetch()
+            ).toJSON()
+                
+            let equipment_id = []
             for (const item of dailyFleet) {
                 for (const list of item.details) {
                     equipment_id.push(list.equip_id)
                 }
             }
-
-
+                        
+                        
+            let data = []
             let equipment = (await Equipment.query().where({aktif: 'Y'}).fetch()).toJSON()
             for (const item of equipment) {
                 if(equipment_id.includes(item.id)){
@@ -115,7 +132,8 @@ class EquipmentApiController {
             let durasi = await diagnoticTime.durasi(t0)
             return response.status(200).json({
                 diagnostic: {
-                    times: durasi, 
+                    times: durasi,
+                    req: dateReq,
                     error: false
                 },
                 data: data
@@ -129,8 +147,8 @@ class EquipmentApiController {
                     error: true,
                     message: error
                 },
-                data: [],
-                equipment_id: equipment_id
+                req: moment(dateReq).format('YYYY-MM-DD HH:mm:ss'),
+                data: []
             })
         }
     }
