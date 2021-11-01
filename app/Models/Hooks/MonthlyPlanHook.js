54ffa0c3c50dac9e30b59f2797f5aaf49eb0bcef
kalleriakronos24 <kalleriakronos24@gmail.com>
2021-11-01 11:00:35 +0800

    fixed monthly plan and daily plans

--- app/Models/Hooks/MonthlyPlanHook.js
+++ app/Models/Hooks/MonthlyPlanHook.js
@@ -1,27 +1,29 @@
-'use strict'
+"use strict";
 
-const db = use('Database')
-const moment = require('moment')
-const DailyPlan = use("App/Models/DailyPlan")
-const MonthlyPlan = use("App/Models/MonthlyPlan")
+const db = use("Database");
+const moment = require("moment");
+const DailyPlan = use("App/Models/DailyPlan");
+const MonthlyPlan = use("App/Models/MonthlyPlan");
 
-const MonthlyPlanHook = exports = module.exports = {}
+const MonthlyPlanHook = (exports = module.exports = {});
 
 MonthlyPlanHook.afterCreate = async (monthlyplan) => {
-    const currentMonthDates = Array.from({length: moment(monthlyplan.month).daysInMonth()},
-    (x, i) => moment().startOf('month').add(i, 'days').format('YYYY-MM-DD'))
-    try {
-        const arr = []
-        for (const item of currentMonthDates) {
-            arr.push({
-                current_date: item,
-                estimate: monthlyplan.estimate / currentMonthDates.length,
-                tipe: monthlyplan.tipe === 'OB' ? 'OB':'COAL',
-                monthlyplans_id: monthlyplan.id
-            })
-        }
-        await DailyPlan.createMany(arr)
-    } catch (error) {
-        console.log(error);
+  const currentMonthDates = Array.from(
+    { length: moment(monthlyplan.month).daysInMonth() },
+    (x, i) => moment(monthlyplan.month).startOf("month").add(i, "days").format("YYYY-MM-DD")
+  );
+  try {
+    for (const item of currentMonthDates) {
+      const dailyPlans = new DailyPlan();
+      dailyPlans.fill({
+        current_date: item,
+        estimate: monthlyplan.estimate / currentMonthDates.length,
+        tipe: monthlyplan.tipe === "OB" ? "OB" : "COAL",
+        monthlyplans_id: monthlyplan.id,
+      });
+      await dailyPlans.save();
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
