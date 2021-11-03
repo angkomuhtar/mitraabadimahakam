97e49f4dc1332772347e347ee549908394e19116
kalleriakronos24 <kalleriakronos24@gmail.com>
2021-11-03 10:22:39 +0800

    ..

--- app/Models/Hooks/DailyRitaseDetailHook.js
+++ app/Models/Hooks/DailyRitaseDetailHook.js
@@ -1,102 +1,149 @@
-'use strict'
+"use strict";
 
-const moment = require('moment')
-const DailyPlan = use("App/Models/DailyPlan")
-const DailyFleet = use("App/Models/DailyFleet")
-const MonthlyPlan = use("App/Models/MonthlyPlan")
-const DailyRitase = use("App/Models/DailyRitase")
-const MasMaterial = use("App/Models/MasMaterial")
-const MasEquipment = use("App/Models/MasEquipment")
-const DailyRitaseDetail = use("App/Models/DailyRitaseDetail")
+const moment = require("moment");
+const DailyPlan = use("App/Models/DailyPlan");
+const DailyFleet = use("App/Models/DailyFleet");
+const MonthlyPlan = use("App/Models/MonthlyPlan");
+const DailyRitase = use("App/Models/DailyRitase");
+const MasMaterial = use("App/Models/MasMaterial");
+const MasEquipment = use("App/Models/MasEquipment");
+const DailyRitaseDetail = use("App/Models/DailyRitaseDetail");
 
-const DailyRitaseDetailHook = exports = module.exports = {}
+const DailyRitaseDetailHook = (exports = module.exports = {});
 
 DailyRitaseDetailHook.beforeInsertData = async (dailyritasedetail) => {
-    const counter = await DailyRitaseDetail.query().where(
-        { hauler_id: dailyritasedetail.hauler_id, dailyritase_id: dailyritasedetail.dailyritase_id }
-    ).getCount()
-    dailyritasedetail.urut = counter + 1
+  const counter = await DailyRitaseDetail.query()
+    .where({
+      hauler_id: dailyritasedetail.hauler_id,
+      dailyritase_id: dailyritasedetail.dailyritase_id,
+    })
+    .getCount();
+  dailyritasedetail.urut = counter + 1;
 
-    const lastData = await DailyRitaseDetail.query().where(
-        { hauler_id: dailyritasedetail.hauler_id, dailyritase_id: dailyritasedetail.dailyritase_id }
-    ).last()
+  const lastData = await DailyRitaseDetail.query()
+    .where({
+      hauler_id: dailyritasedetail.hauler_id,
+      dailyritase_id: dailyritasedetail.dailyritase_id,
+    })
+    .last();
 
-    dailyritasedetail.duration = lastData ? moment.duration(moment().diff(moment(lastData.check_in))).as('minutes') : -1
+  dailyritasedetail.duration = lastData
+    ? moment.duration(moment().diff(moment(lastData.check_in))).as("minutes")
+    : -1;
 
-    const dailyRitase = await DailyRitase.findOrFail(dailyritasedetail.dailyritase_id)
-    const totalRitase = await DailyRitaseDetail.query().where('dailyritase_id', dailyritasedetail.dailyritase_id).getCount()
-    dailyRitase.merge({tot_ritase: totalRitase})
-    await dailyRitase.save()
-}
+  const dailyRitase = await DailyRitase.findOrFail(
+    dailyritasedetail.dailyritase_id
+  );
+  const totalRitase = await DailyRitaseDetail.query()
+    .where("dailyritase_id", dailyritasedetail.dailyritase_id)
+    .getCount();
+  dailyRitase.merge({ tot_ritase: totalRitase });
+  await dailyRitase.save();
+};
 
 DailyRitaseDetailHook.afterInsertData = async (dailyritasedetail) => {
-    const dailyRitase = await DailyRitase.findOrFail(dailyritasedetail.dailyritase_id)
-    const totalRitase = await DailyRitaseDetail.query().where('dailyritase_id', dailyritasedetail.dailyritase_id).getCount()
-    dailyRitase.merge({tot_ritase: totalRitase})
-    await dailyRitase.save()
+  const dailyRitase = await DailyRitase.findOrFail(
+    dailyritasedetail.dailyritase_id
+  );
+  const totalRitase = await DailyRitaseDetail.query()
+    .where("dailyritase_id", dailyritasedetail.dailyritase_id)
+    .getCount();
+  dailyRitase.merge({ tot_ritase: totalRitase });
+  await dailyRitase.save();
 
-    /* GET CAPACITY HAULER */
-    const hauler = await MasEquipment.findOrFail(dailyritasedetail.hauler_id)
+  /* GET HAULER TYPE (SFI OR REGULAR OHT )*/
+  const hauler = await MasEquipment.findOrFail(dailyritasedetail.hauler_id);
 
-    /* GET VOLUME MATERIAL */
-    const volume = await MasMaterial.query().where('id', dailyRitase.material).last()
+  /* GET VOLUME MATERIAL */
+  const volume = await MasMaterial.query()
+    .where("id", dailyRitase.material)
+    .last();
 
-    /* GET PIT ID */
-    const dailyFleet = await DailyFleet.query().where('id', dailyRitase.dailyfleet_id).last()
+  /* GET PIT ID */
+  const dailyFleet = await DailyFleet.query()
+    .where("id", dailyRitase.dailyfleet_id)
+    .last();
 
-    /* GET MONTLY PLAN */
-    const montlyPlan = await MonthlyPlan.query().where(w => {
-            w.where('pit_id', dailyFleet.pit_id)
-            w.where('tipe', 'OB')
-        }
-    ).last()
-
-    /* GET PLAN DATE */
-    const date = moment(dailyritasedetail.check_in).format('YYYY-MM-DD')
-    const dailyPlan = await DailyPlan.query().where(w => {
-            w.where('current_date', date)
-            w.where('monthlyplans_id', montlyPlan.id)
-        }
-    ).first()
-    dailyPlan.merge({
-        actual: parseFloat(dailyPlan.actual) + parseFloat(volume.vol)
+  /* GET MONTLY PLAN */
+  const montlyPlan = await MonthlyPlan.query()
+    .where((w) => {
+      w.where("pit_id", dailyFleet.pit_id);
+      w.where("tipe", "OB");
     })
-    await dailyPlan.save()
-}
+    .last();
+
+  /* GET PLAN DATE */
+  const date = moment(dailyritasedetail.check_in).format("YYYY-MM-DD");
+  const dailyPlan = await DailyPlan.query()
+    .where((w) => {
+      w.where("current_date", date);
+      w.where("monthlyplans_id", montlyPlan.id);
+    })
+    .first();
+
+  /** check whether this is a OHT or SFI */
+  const regTest = /SFI/i;
+  const equipmentName = hauler.kode;
+  const checkIfSFIType = regTest.test(equipmentName);
+
+  if (checkIfSFIType) {
+    dailyPlan.merge({
+      actual: parseFloat(dailyPlan.actual) + parseFloat(hauler.qty_capacity),
+    });
+  }
+
+  dailyPlan.merge({
+    actual: parseFloat(dailyPlan.actual) + parseFloat(volume.vol),
+  });
+  
+  await dailyPlan.save();
+};
 
 DailyRitaseDetailHook.afterDeleteData = async (dailyritasedetail) => {
+  const dailyRitase = await DailyRitase.findOrFail(
+    dailyritasedetail.dailyritase_id
+  );
+  const totalRitase = await DailyRitaseDetail.query()
+    .where("dailyritase_id", dailyritasedetail.dailyritase_id)
+    .getCount();
+  dailyRitase.merge({ tot_ritase: totalRitase });
+  await dailyRitase.save();
 
-    const dailyRitase = await DailyRitase.findOrFail(dailyritasedetail.dailyritase_id)
-    const totalRitase = await DailyRitaseDetail.query().where('dailyritase_id', dailyritasedetail.dailyritase_id).getCount()
-    dailyRitase.merge({tot_ritase: totalRitase})
-    await dailyRitase.save()
+  /* GET CAPACITY HAULER */
+  const hauler = await MasEquipment.findOrFail(dailyritasedetail.hauler_id);
 
-    /* GET CAPACITY HAULER */
-    const hauler = await MasEquipment.findOrFail(dailyritasedetail.hauler_id)
+  /* GET VOLUME MATERIAL */
+  const volume = await MasMaterial.query()
+    .where("id", dailyRitase.material)
+    .last();
 
-    /* GET VOLUME MATERIAL */
-    const volume = await MasMaterial.query().where('id', dailyRitase.material).last()
-
-    /* GET PLAN DATE */
-    const date = moment(dailyritasedetail.check_in).format('YYYY-MM-DD')
-    const dailyPlan = await DailyPlan.query().where('current_date', date).first()
-    dailyPlan.merge({
-        actual: parseFloat(dailyPlan.actual) - parseFloat(volume.vol)
-    })
-    await dailyPlan.save()
-}
-
+  /* GET PLAN DATE */
+  const date = moment(dailyritasedetail.check_in).format("YYYY-MM-DD");
+  const dailyPlan = await DailyPlan.query().where("current_date", date).first();
+  dailyPlan.merge({
+    actual: parseFloat(dailyPlan.actual) - parseFloat(volume.vol),
+  });
+  await dailyPlan.save();
+};
 
 DailyRitaseDetailHook.beforeUpdateData = async (dailyritasedetail) => {
-    const lastData = await DailyRitaseDetail.query().where(
-        { hauler_id: dailyritasedetail.hauler_id, dailyritase_id: dailyritasedetail.dailyritase_id }
-    ).last()
+  const lastData = await DailyRitaseDetail.query()
+    .where({
+      hauler_id: dailyritasedetail.hauler_id,
+      dailyritase_id: dailyritasedetail.dailyritase_id,
+    })
+    .last();
 
-    dailyritasedetail.duration = lastData ? moment.duration(moment().diff(moment(lastData.check_in))).as('minutes') : -1
+  dailyritasedetail.duration = lastData
+    ? moment.duration(moment().diff(moment(lastData.check_in))).as("minutes")
+    : -1;
 
-    const dailyRitase = await DailyRitase.findOrFail(dailyritasedetail.dailyritase_id)
-    const totalRitase = await DailyRitaseDetail.query().where('dailyritase_id', dailyritasedetail.dailyritase_id).getCount()
-    dailyRitase.merge({tot_ritase: totalRitase})
-    await dailyRitase.save()
-
-}
+  const dailyRitase = await DailyRitase.findOrFail(
+    dailyritasedetail.dailyritase_id
+  );
+  const totalRitase = await DailyRitaseDetail.query()
+    .where("dailyritase_id", dailyritasedetail.dailyritase_id)
+    .getCount();
+  dailyRitase.merge({ tot_ritase: totalRitase });
+  await dailyRitase.save();
+};
