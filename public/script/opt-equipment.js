69ec462af99057843aebd3173fc42f54a24c3573
Mitra Abadi Mahakam <>
2021-12-22 11:36:46 +0800

    mam issue

---
+++ public/script/opt-equipment.js
@@ -0,0 +1,41 @@
+$(function(){
+    console.log('script/opt-equipment');
+
+    $('select').select2()
+
+    $('body select.xselect2equipment').each(function(){
+        var values = $(this).data('check')
+        var elm = $(this)
+        elm.children().remove()
+        $.ajax({
+            async: true,
+            url: '/ajax/equipment?selected='+values,
+            method: 'GET',
+            success: function(result){
+                if(result.length > 0){
+                    setSelected(result, values)
+                    elm.html(result.map( v => '<option value="'+v.id+'" '+v.selected+'>' +v.kode+ ' | '+v.unit_model+'</option>'))
+                    initSelected(result, elm)
+                    elm.trigger('change');
+                }else{
+                    elm.html('<option value="" selected>Blum ada data...</option>')
+                }
+            },
+            error: function(err){
+                console.log(err);
+            }
+        })
+    })
+
+    function setSelected(list, value){
+        let data = list.map(elm => elm.id === value ? {...elm, selected: 'selected'} : {...elm, selected: ''})
+        return data
+    }
+    
+    function initSelected(data, elm){
+        var lenSelected = data.filter( a => a.selected === 'selected')
+        if(!lenSelected.length > 0){
+            elm.prepend('<option value="" selected>Pilih</option>')
+        }
+    }
+})
