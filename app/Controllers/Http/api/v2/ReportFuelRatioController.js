b8d8e9f87ceac8fe6ad00d91b5fbf5655923b09c
Ayat Ekapoetra <ayat.ekapoetra@gmail.com>
2022-08-02 14:46:29 +0800

    rev. pesan error jika Daily plan tidak di temukan pada upload data Ritase OB

--- app/Controllers/Http/api/v2/ReportFuelRatioController.js
+++ app/Controllers/Http/api/v2/ReportFuelRatioController.js
@@ -15,7 +15,7 @@ class ReportFuelRatioController {
         let durasi
         var t0 = performance.now()
         var req = request.all()
-        console.log(req);
+        // console.log(req);
         const user = await userValidate(auth)
         if(!user){
             return response.status(403).json({
@@ -27,15 +27,119 @@ class ReportFuelRatioController {
             })
         }
         req.colorGraph = [ '#7ab2fa', '#1074f7', '#0451b6' ]
+
+        console.log('====================================');
+        console.log(moment(req.start).format('YYYY-[W]ww'));
+        console.log(moment(req.end).format('YYYY-[W]ww'));
+        console.log('====================================');
+
+        if(req.inp_ranges === 'week'){
+            req.start = moment(req.start).format('YYYY-[W]ww')
+            req.end = moment(req.end).format('YYYY-[W]ww')
+        }
         
         if(req.range_type === 'pit'){
-            const data = await ReportFuelRatioHelpers.PIT_WISE(req)
-            console.log(data);
-            return data
+            try {
+                const data = await ReportFuelRatioHelpers.PIT_WISE(req)
+                let resultPIT = {
+                    site: data.site,
+                    pit: data.pit,
+                    staticRatio: data.staticRatio,
+                    dataCurrent: data.series.map((val, c) => {
+                        return {
+                            name: val.name,
+                            frontColor: req.colorGraph[c],
+                            values: val.data.map((obj, i) => {
+                                return {
+                                    value: obj,
+                                    label: data.xAxis[i]
+                                }
+                            })
+                        }
+                    }),
+                    dataCummulative: data.cummSeries?.map(val => {
+                        return {
+                            name: val.name,
+                            values: val.data.map((obj, i) => {
+                                return {
+                                    value: obj,
+                                    label: data.cummxAxis[i]
+                                }
+                            })
+                        }
+                    })
+                }
+                durasi = await diagnoticTime.durasi(t0);
+                return response.status(200).json({
+                    diagnostic: {
+                        ver: version,
+                        times: durasi,
+                        error: false,
+                    },
+                    data: resultPIT,
+                });
+            } catch (error) {
+                durasi = await diagnoticTime.durasi(t0);
+                return response.status(403).json({
+                    diagnostic: {
+                        ver: version,
+                        times: durasi,
+                        error: true,
+                        message: error,
+                    },
+                    data: [],
+                });
+            }
         }else{
-            const data = await ReportFuelRatioHelpers.PERIODE_WISE(req)
-            console.log(data);
-            return data
+            try {
+                const data = await ReportFuelRatioHelpers.PERIODE_WISE(req)
+                let resultPeriode = {
+                    site: data.site,
+                    dataCurrent: data.series.map((val, c) => {
+                        return {
+                            name: val.name,
+                            frontColor: req.colorGraph[c],
+                            values: val.data.map((obj, i) => {
+                                return {
+                                    value: obj,
+                                    label: data.xAxis[i]
+                                }
+                            })
+                        }
+                    }),
+                    dataCummulative: data.cummSeries?.map(val => {
+                        return {
+                            name: val.name,
+                            values: val.data.map((obj, i) => {
+                                return {
+                                    value: obj,
+                                    label: data.cummxAxis[i]
+                                }
+                            })
+                        }
+                    })
+                }
+                durasi = await diagnoticTime.durasi(t0);
+                return response.status(200).json({
+                    diagnostic: {
+                        ver: version,
+                        times: durasi,
+                        error: false,
+                    },
+                    data: resultPeriode,
+                });
+            } catch (error) {
+                durasi = await diagnoticTime.durasi(t0);
+                return response.status(403).json({
+                    diagnostic: {
+                        ver: version,
+                        times: durasi,
+                        error: true,
+                        message: error,
+                    },
+                    data: [],
+                });
+            }
         }
     }
 }
